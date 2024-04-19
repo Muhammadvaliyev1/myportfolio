63fa25c56b6f757b8e1b977d9e78cb279837c511
Muhammadvaliyev1 <127742778+Muhammadvaliyev1@users.noreply.github.com>
2024-04-19 18:45:39 +0500

    added new details

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,24 @@ module.exports = {
       },
       screens: {
         'base': '1210px',
-        'medium': '1110px'
+        'medium': '1110px',
+        'mdd': '784px',
+        'smm': '700px'
       },
       colors: {
         'darkgray': '#232323',
         'space-gray': '#2A2A29',
+        'spaced-gray': '#191919',
+        'white-gray': '#656565',
         'yellow': '#EAC566',
         'space-yellow': '#EBE3CE',
         'green': '#00713D',
         'skyblue': '#1F6AAD',
         'wheat': '#E9E9EB',
         'space-wheat': '#EBE3CE',
+      },
+      padding: {
+        '5.5': '22px'
       }
     },
   },
